0d8dc20387e3ad138f4c946c17eab557d34b83ba
Benjamin Menetrier <benjamin.menetrier@met.no>
2023-05-24 20:14:32 +0200

    Working

--- ecmwf.sh
+++ ecmwf.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+# Remember path
+PWD_INIT=${PWD}
+
 # Update or clone OOPS-JEDI repo
+GIT_BRANCH=feature/use_oops_ecmwf
+echo -e "-- Clone OOPS-JEDI repository, branch ${GIT_BRANCH}"
 if test -d $1/oops-jcsda; then
   cd $1/oops-jcsda
-  git checkout develop
+  git checkout ${GIT_BRANCH}
   git pull
+  cd ${PWD_INIT}
 else
-  git clone https://github.com/jcsda/oops $1/oops-jcsda
+  git clone https://github.com/jcsda-internal/oops $1/oops-jcsda
+  cd $1/oops-jcsda
+  git checkout ${GIT_BRANCH}
+  git pull
+  cd ${PWD_INIT}
 fi
 
 # Copy cmake
@@ -14,22 +24,18 @@ if test ! -d $1/cmake; then
   cp -fr $1/oops-jcsda/cmake $1
 fi
 
-# Copy cmake
+# Copy tools
 if test ! -d $1/tools; then
   cp -fr $1/oops-jcsda/tools $1
 fi
 
 # Copy src and rename src_tmp/oops into src_tmp/oopsjedi
 rm -fr $1/src_tmp
-cp -fr $1/oops-jcsda/src $1/src_tmp
-mv $1/src_tmp/oops $1/src_tmp/oopsjedi
+mkdir -p $1/src_tmp
+cp -fr $1/oops-jcsda/src/oops $1/src_tmp/oopsjedi
+cp -f $1/oops-jcsda/src/CMakeLists.txt $1/src_tmp/CMakeLists.txt
 
-# Update src_tmp/CMakeLists.txt
-sed -i -e s:"oops/":"oopsjedi/":g $1/src_tmp/CMakeLists.txt
-sed -i -e s:"/oops":"/oopsjedi":g $1/src_tmp/CMakeLists.txt
-sed -i -e s:"oopsjedijedi":"oopsjedi/":g $1/src_tmp/CMakeLists.txt
-
-# Copy src_tmp into src
+# Copy src_tmp/oopsjedi into src/oopsjedi to get the correct tree
 if test ! -d $1/src; then 
   cp -fr $1/src_tmp $1/src
 fi
@@ -50,21 +56,28 @@ for file in `find $1/src_tmp -name *.h -o -name *.cc -o -name *.F90`; do
          -e s:"OOPSJEDI_ABSTRACT":"OOPS_ABSTRACT":g \
          -e s:"OOPSJEDI_CONCRETE":"OOPS_CONCRETE":g \
          -e s:"OOPSJEDI_UTIL":"OOPS_UTIL":g \
-           ${file}
-done
-
-# Link extension files
-for file in `find $1/extension -name *.h -o -name *.cc -o -name *.F90 -name *.txt`; do
-  ln -sf ${file} ${file/extension/src_tmp}
+         ${file}
 done
 
 # Copy modified files
+echo -e "-- Copy modified files (different from their src counterpart)"
 for file in `find $1/src_tmp -name *.h -o -name *.cc -o -name *.F90`; do
   if ! cmp -s "${file}" "${file/src_tmp/src}"; then
-    echo -e " - File "${file}" is different from "${file/src_tmp/src}
+    echo -e "--  + "${file}
+    rm -f ${file/src_tmp/src}
     cp -f ${file} ${file/src_tmp/src}
   fi
 done
+
+# Link extension files
+echo -e "-- Link extension files"
+rm -f $1/diff.txt
+for file in `find $1/extension -name *.h -o -name *.cc -o -name *.F90` "$1/extension/CMakeLists.txt"; do  
+  echo -e "--  + "${file}
+  diff --color ${file} ${file/extension/src_tmp} >> $1/diff.txt
+  ln -sf ${file} ${file/extension/src}
+done
+echo -e "-- Diff available in: $1/diff.txt"
 
 # Cleaning
 rm -fr $1/src_tmp
---
+++ extension/oopsjedi/base/Increment.h
@@ -0,0 +1,283 @@
+/*
+ * (C) Copyright 2009-2016 ECMWF.
+ * (C) Copyright 2017-2021 UCAR.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef OOPSJEDI_BASE_INCREMENT_H_
+#define OOPSJEDI_BASE_INCREMENT_H_
+
+#include <memory>
+#include<string>
+#include<vector>
+
+#include "atlas/field.h"
+
+#include "oopsjedi/base/Geometry.h"
+#include "oopsjedi/base/State.h"
+#include "oopsjedi/base/Variables.h"
+#include "oopsjedi/interface/Increment.h"
+#include "oopsjedi/mpi/mpi.h"
+#include "oopsjedi/util/DateTime.h"
+#include "oopsjedi/util/gatherPrint.h"
+#include "oopsjedi/util/Timer.h"
+
+namespace oopsjedi {
+
+// -----------------------------------------------------------------------------
+/// \brief Increment class used in oops
+///
+/// \details
+/// Adds extra methods that do not need to be implemented in the model implementations:
+/// - timeComm()  (accessor to the MPI communicator in time - collection of processes
+///                holding the data needed to represent the state in a particular region
+///                of space X_i and throughout the whole time interval for which DA is done)
+/// - shift_forward
+/// - shift_backward
+/// - toAtlas, atlas
+///
+/// Adds communication through time to the following Increment methods:
+/// - dot_product_with
+/// - norm
+/// - print
+
+template <typename MODEL>
+class Increment : public interface::Increment<MODEL> {
+  typedef oops::Increment<MODEL>     Increment_;
+  typedef Geometry<MODEL>            Geometry_;
+
+ public:
+  /// Constructor for specified \p geometry, with \p variables, valid on \p date
+  Increment(const Geometry_ & geometry, const Variables & variables, const utiljedi::DateTime & date);
+  /// Copies \p other increment, changing its resolution to \p geometry
+  Increment(const Geometry_ & geometry, const Increment & other);
+  /// Creates Increment with the same geometry and variables as \p other.
+  /// Copies \p other if \p copy is true, otherwise creates zero increment
+  Increment(const Increment & other, const bool copy = true);
+
+  /// Constructor from an oops::State<MODEL> and the corresponding oopsjedi::Geometry<MODEL>
+  Increment(const Increment_ &, const Geometry_ &);
+
+  Increment & operator=(const Increment &);
+
+  /// Accessor to geometry associated with this Increment
+  const Geometry_ & geometry() const {return resol_;}
+
+  /// Accessor to the time communicator
+  const eckit::mpi::Comm & timeComm() const {return *timeComm_;}
+
+  /// Shift forward in time by \p dt
+  void shift_forward(const utiljedi::DateTime & dt);
+  /// Shift backward in time by \p dt
+  void shift_backward(const utiljedi::DateTime & dt);
+
+  /// Accessors to the ATLAS fieldset
+  const atlas::FieldSet & fieldSet() const;
+  atlas::FieldSet & fieldSet();
+  void synchronizeFields();
+  void synchronizeFieldsAD();
+
+  /// dot product with the \p other increment
+  double dot_product_with(const Increment & other) const;
+  /// Norm for diagnostics
+  double norm() const;
+
+ private:
+  void print(std::ostream &) const override;
+
+  const Geometry_ & resol_;
+  const eckit::mpi::Comm * timeComm_;  /// pointer to the MPI communicator in time
+};
+
+// -----------------------------------------------------------------------------
+
+template <typename MODEL>
+Increment<MODEL>::Increment(const Geometry_ & geometry, const Variables & variables,
+                            const utiljedi::DateTime & date):
+  interface::Increment<MODEL>(geometry, variables, date), resol_(geometry),
+  timeComm_(&geometry.timeComm())
+{}
+
+// -----------------------------------------------------------------------------
+
+template <typename MODEL>
+Increment<MODEL>::Increment(const Geometry_ & geometry, const Increment & other):
+  interface::Increment<MODEL>(geometry, other), resol_(geometry),
+  timeComm_(other.timeComm_)
+{}
+
+// -----------------------------------------------------------------------------
+
+template <typename MODEL>
+Increment<MODEL>::Increment(const Increment & other, const bool copy):
+  interface::Increment<MODEL>(other, copy), resol_(other.resol_),
+  timeComm_(other.timeComm_)
+{}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+Increment<MODEL>::Increment(const Increment_ & increment, const Geometry_ & resol) :
+  interface::Increment<MODEL>(increment), resol_(resol), timeComm_(&resol.timeComm())
+{}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+Increment<MODEL> & Increment<MODEL>::operator=(const Increment & rhs) {
+  ASSERT(resol_ == rhs.resol_);
+  ASSERT(timeComm_ == rhs.timeComm_);
+  interface::Increment<MODEL>::operator=(rhs);
+  return *this;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+double Increment<MODEL>::dot_product_with(const Increment & dx) const {
+  double zz = interface::Increment<MODEL>::dot_product_with(dx);
+  timeComm_->allReduceInPlace(zz, eckit::mpi::Operation::SUM);
+  return zz;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+double Increment<MODEL>::norm() const {
+  double zz = interface::Increment<MODEL>::norm();
+  zz *= zz;
+  timeComm_->allReduceInPlace(zz, eckit::mpi::Operation::SUM);
+  zz = sqrt(zz);
+  return zz;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+void Increment<MODEL>::shift_forward(const utiljedi::DateTime & begin) {
+  Log::trace() << "Increment<MODEL>::Increment shift_forward starting" << std::endl;
+  static int tag = 159357;
+  size_t mytime = timeComm_->rank();
+
+// Send values of M.dx_i at end of my subwindow to next subwindow
+  if (mytime + 1 < timeComm_->size()) {
+    oopsjedi::mpi::send(*timeComm_, *this, mytime+1, tag);
+  }
+
+// Receive values at beginning of my subwindow from previous subwindow
+  if (mytime > 0) {
+    oopsjedi::mpi::receive(*timeComm_, *this, mytime-1, tag);
+  } else {
+    this->zero(begin);
+  }
+
+  ++tag;
+  Log::trace() << "Increment<MODEL>::Increment shift_forward done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+void Increment<MODEL>::shift_backward(const utiljedi::DateTime & end) {
+  Log::trace() << "Increment<MODEL>::Increment shift_backward starting" << std::endl;
+  static int tag = 30951;
+  size_t mytime = timeComm_->rank();
+
+// Send values of dx_i at start of my subwindow to previous subwindow
+  if (mytime > 0) {
+    oopsjedi::mpi::send(*timeComm_, *this, mytime-1, tag);
+  }
+
+// Receive values at end of my subwindow from next subwindow
+  if (mytime + 1 < timeComm_->size()) {
+    oopsjedi::mpi::receive(*timeComm_, *this, mytime+1, tag);
+  } else {
+    this->zero(end);
+  }
+
+  ++tag;
+  Log::trace() << "Increment<MODEL>::Increment shift_backward done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+const atlas::FieldSet & Increment<MODEL>::fieldSet() const {
+  if (interface::Increment<MODEL>::fset_.empty()) {
+    interface::Increment<MODEL>::fset_ = atlas::FieldSet();
+    this->toFieldSet(interface::Increment<MODEL>::fset_);
+    for (const auto & field : interface::Increment<MODEL>::fset_) {
+      ASSERT_MSG(field.rank() == 2,
+                 "OOPS expects the model's Increment::toFieldSet method to return rank-2 fields,"
+                 " but field " + field.name() + " has rank = " + std::to_string(field.rank()));
+    }
+  }
+  return interface::Increment<MODEL>::fset_;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+atlas::FieldSet & Increment<MODEL>::fieldSet() {
+  if (interface::Increment<MODEL>::fset_.empty()) {
+    interface::Increment<MODEL>::fset_ = atlas::FieldSet();
+    this->toFieldSet(interface::Increment<MODEL>::fset_);
+    for (const auto & field : interface::Increment<MODEL>::fset_) {
+      ASSERT_MSG(field.rank() == 2,
+                 "OOPS expects the model's Increment::toFieldSet method to return rank-2 fields,"
+                 " but field " + field.name() + " has rank = " + std::to_string(field.rank()));
+    }
+  }
+  return interface::Increment<MODEL>::fset_;
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+void Increment<MODEL>::synchronizeFields() {
+  // TODO(JEDI core team): remove this method when accessors are fully implemented
+  ASSERT(!interface::Increment<MODEL>::fset_.empty());
+  this->fromFieldSet(interface::Increment<MODEL>::fset_);
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+void Increment<MODEL>::synchronizeFieldsAD() {
+  // TODO(JEDI core team): remove this method when accessors are fully implemented
+  if (!interface::Increment<MODEL>::fset_.empty()) {
+    this->toFieldSetAD(interface::Increment<MODEL>::fset_);
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+template<typename MODEL>
+void Increment<MODEL>::print(std::ostream & os) const {
+  if (timeComm_->size() > 1) {
+    utiljedi::gatherPrint(os, this->increment(), *timeComm_);
+  } else {
+    os << this->increment();
+  }
+}
+
+// -----------------------------------------------------------------------------
+/// Add on \p dx incrment to model state \p xx
+template <typename MODEL>
+State<MODEL> & operator+=(State<MODEL> & xx, const Increment<MODEL> & dx) {
+  Log::trace() << "operator+=(State, Increment) starting" << std::endl;
+  utiljedi::Timer timer("oopsjedi::Increment", "operator+=(State, Increment)");
+  xx.state() += dx.increment();
+  Log::trace() << "operator+=(State, Increment) done" << std::endl;
+  return xx;
+}
+
+
+}  // namespace oopsjedi
+
+#endif  // OOPSJEDI_BASE_INCREMENT_H_
